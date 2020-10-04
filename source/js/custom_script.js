486fda169ef9af2a9eb04c912ac8cdcb453ff442
HangPT <hangpt@ubuntu.ubuntu-domain>
2020-10-04 18:15:04 +0700

    update

--- source/js/custom_script.js
+++ source/js/custom_script.js
@@ -1,16 +1,25 @@
-    function readURL(input) {
-        if (input.files && input.files[0]) {
-            let reader = new FileReader();
+function readURL(input) {
+    if (input.files && input.files[0]) {
+        let reader = new FileReader();
 
-            reader.onload = function(e) {
-                $('.preview').attr('src', e.target.result);
-            }
-            
-            reader.readAsDataURL(input.files[0]);
+        reader.onload = function(e) {
+            $('.preview').attr('src', e.target.result);
         }
+        
+        reader.readAsDataURL(input.files[0]);
     }
+}
 
-    $('.image-input').change(function() {
-        readURL(this);
-    });
+$('.image-input').change(function() {
+    readURL(this);
+});
+$('.checkbox-row').change(function () {
+    let input = this;
+    let container = $(this).parents('.noti-item');
+    if ($(this).is(':checked')) {
+        container.addClass('picked-row');
+    } else {
+        container.removeClass('picked-row');
+    }
+});
     
