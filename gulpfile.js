b386e9cd61684c60a3e6b02834a48710c238c650
HangPT <hangpt@ubuntu.ubuntu-domain>
2021-04-07 18:58:47 +0700

    update

--- gulpfile.js
+++ gulpfile.js
@@ -4,22 +4,41 @@ var cssnano = require('cssnano');
 var concat = require('gulp-concat');
 
 gulp.task('css', async function() {
-    return gulp.src(['node_modules/bootstrap/dist/css/bootstrap.min.css', 'source/css/*.css'])
+    return gulp.src([
+            'node_modules/bootstrap/dist/css/bootstrap.min.css',
+            'node_modules/chart.js/dist/Chart.min.css',
+            'source/css/*.css'
+        ])
         // .pipe(cssnano())
         .pipe(concat('main.css'))
         .pipe(gulp.dest('source/assets/css/'))
         .pipe(gulp.dest('../resources/assets/css/'))
 });
 gulp.task('minify', async function () {
-    return gulp.src(['node_modules/jquery/dist/jquery.min.js', 'node_modules/bootstrap/dist/js/bootstrap.min.js', 'source/js/*.js'])
+    return gulp.src([
+            'node_modules/moment/min/moment.min.js',
+            'node_modules/jquery/dist/jquery.min.js',
+            'node_modules/bootstrap/dist/js/bootstrap.min.js',
+            'node_modules/chart.js/dist/Chart.min.js',
+            'source/js/*.js',
+            'node_modules/jquery-ui-dist/jquery-ui.js',
+    ])
         // .pipe(uglify())
         .pipe(concat('script.js'))
+        .pipe(gulp.dest('source/assets/js/'))
+        .pipe(gulp.dest('../resources/assets/js/'))
+});
+gulp.task('cpjs', async function () {
+    return gulp.src([
+            'source/js/custom_script.js'])
+        .pipe(concat('modules_script.js'))
         .pipe(gulp.dest('source/assets/js/'))
         .pipe(gulp.dest('../resources/assets/js/'))
 });
 gulp.task('default', function(cb) {
     gulp.series(
         'css',
-        'minify'
+        'minify',
+        'cpjs'
     )(cb);
 });
